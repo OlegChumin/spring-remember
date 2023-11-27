759d06848834d6265d19ec16a0dbd3de3671c75c
Чумин Олег <oleg.chumin@gazprombank.ru>
2023-11-27 17:17:17 +0300

    Add changes in Test5.java

--- src/main/java/spring_introduction_5/Test5.java
+++ src/main/java/spring_introduction_5/Test5.java
@@ -8,6 +8,9 @@ public class Test5 {
                 new ClassPathXmlApplicationContext("applicationContext5.xml");
         Dog myDog = context.getBean("myPet", Dog.class);
         myDog.say();
+
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.say();
         context.close(); // -> destroy method
     }
 }
